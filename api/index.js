bde8164c09e0cb439e594c0d60b26274691a2ca9
Lora Violet <priyanthakumarasiri1970@gmail.com>
2026-02-27 21:31:58 +0530

    Last Changes

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import locationRoute from '../routes/locationRoute.js'
+
+dotenv.config()
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 4000
+const MONGO_URI = process.env.MONGO_URI
+
+// Routes
+app.use('/', locationRoute)
+
+// Database Connection
+mongoose.connect(MONGO_URI)
+    .then(() => console.log("Database Connected"))
+    .catch((err) => console.log(err))
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ routes/locationRoute.js
@@ -0,0 +1,17 @@
+import express from "express";
+import Location from "../modules/locationModule.js";
+
+const locationRoute = express.Router();
+
+locationRoute.post('/api/location', async (req, res) => {
+    try {
+        const data = req.body;
+        const location = new Location(data);
+        await location.save();
+        res.status(200).json({ message: 'Location saved successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to fetch location', error: error.message })
+    }
+})
+
+export default locationRoute
